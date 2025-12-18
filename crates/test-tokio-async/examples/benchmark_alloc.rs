22dbfc46d8207391c2423c17faf147925a553b74
pawurb <contact@pawelurbanek.com>
2025-12-19 00:05:18 +0100

    Tweak benchmarks

--- crates/test-tokio-async/examples/benchmark_alloc.rs
+++ crates/test-tokio-async/examples/benchmark_alloc.rs
@@ -1,12 +1,24 @@
+use std::time::{Duration, Instant};
+
+const WAIT_FOR: Duration = Duration::from_micros(1);
+
+// This function is used to spin-wait for a given duration.
+// It's more precise than std::thread::sleep.
+fn spin_wait() {
+    let start = Instant::now();
+    while start.elapsed() < WAIT_FOR {}
+}
+
 #[hotpath::measure]
 fn alloc() {
-    let vec = vec![1u8; 1024];
+    let vec = vec![1u8; 128];
     std::hint::black_box(vec);
+    spin_wait();
 }
 
 #[hotpath::main]
 fn main() {
-    for _ in 0..1_000_000 {
+    for _ in 0..100_000 {
         alloc();
     }
 }
--- crates/test-tokio-async/examples/benchmark_noop.rs
+++ crates/test-tokio-async/examples/benchmark_noop.rs
@@ -1,9 +1,18 @@
+use std::time::{Duration, Instant};
+
+const WAIT_FOR: Duration = Duration::from_micros(1);
+
+// This function is used to spin-wait for a given duration.
+// It's more precise than std::thread::sleep.
 #[hotpath::measure]
-fn noop() {}
+fn spin_wait() {
+    let start = Instant::now();
+    while start.elapsed() < WAIT_FOR {}
+}
 
 #[hotpath::main]
 fn main() {
-    for _ in 0..1_000_000 {
-        noop();
+    for _ in 0..100_000 {
+        spin_wait();
     }
 }
